5b30e7c1ea1626e8a8fc6eaa6eb5734845c8e2f7
Domanat <maxim.tomilko@yandex.ru>
2020-05-29 17:23:27 +0300

    Add insertion in database

--- handlers.go
+++ handlers.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
 	"regexp"
 
+	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/gorilla/sessions"
 )
 
+type User struct {
+	Name         string
+	Number       string
+	Passport     string
+	Gender       string
+	Email        string
+	BirthDate    string
+	RegAddr      string
+	ActualAddr   string
+	DeliveryAddr string
+}
+
 var (
+	db    *sql.DB
 	key   = []byte("GuwJCl9cktJBgTwt8lHuce6gr1UY6u7V")
 	store = sessions.NewCookieStore(key)
 )
@@ -31,19 +48,59 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func isInDatabase(user User) bool {
+	query := fmt.Sprintf("SELECT * FROM users WHERE name='%s' OR email='%s' OR number='%s' OR registAddress", user.Name, user.Email, user.RegAddr)
+
+	rows, err := db.Query(query)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if rows.Next() {
+		return true
+	} else {
+		return false
+	}
+}
+
 func logup(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "UserCookie")
 	session.Values["authenticated"] = false
 	session.Save(r, w)
 
-	tmpl := template.Must(template.ParseFiles("/templates/logup.html"))
+	tmpl := template.Must(template.ParseFiles("logup.html"))
 
 	if r.Method == "POST" {
-		//read data and write in database
+		//read data and write in the database
 		//validate data
-		//if everything correct then make session
-		session.Values["authenticated"] = true
-		session.Save(r, w)
+		var user User
+		user.Name = r.FormValue("name")
+		user.Number = r.FormValue("number")
+		user.Passport = r.FormValue("passport")
+		user.Gender = r.FormValue("gender")
+		user.Email = r.FormValue("email")
+		user.BirthDate = r.FormValue("data")
+		user.RegAddr = r.FormValue("registAddress")
+		user.ActualAddr = r.FormValue("actualAddress")
+		user.DeliveryAddr = r.FormValue("deliveryAddress")
+
+		if !isValidEmail(user.Email) || isInDatabase(user) {
+			w.WriteHeader(http.StatusUnauthorized)
+		} else {
+
+			query := fmt.Sprintf("INSERT INTO users(name, number, passport, date, sex, email, registAddress, actualAddress, deliveryAddress) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
+				user.Name, user.Number, user.Passport, user.BirthDate, user.Gender, user.Email, user.RegAddr, user.ActualAddr, user.DeliveryAddr)
+
+			rows, err := db.Query(query)
+			if err != nil {
+				panic(err)
+			}
+
+			defer rows.Close()
+
+			http.Redirect(w, r, "/thanks", http.StatusTemporaryRedirect)
+		}
+
 	}
 
 	tmpl.Execute(w, nil)
@@ -51,7 +108,7 @@ func logup(w http.ResponseWriter, r *http.Request) {
 
 func login(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "UserCookie")
-	tmpl := template.Must(template.ParseFiles("/templates/login.html"))
+	tmpl := template.Must(template.ParseFiles("login.html"))
 
 	if r.Method == "POST" {
 		//search in database and validate data
