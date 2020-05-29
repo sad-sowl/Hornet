9aebfc134ffbc0eaedea99be8a87527d4e284879
Domanat <maxim.tomilko@yandex.ru>
2020-05-29 17:23:46 +0300

    Open database

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+
+	var err error
+	db, err = sql.Open("mysql", "root:Qwerty0106@tcp(127.0.0.1:3306)/hornet")
+	if err != nil {
+		panic(err)
+	}
+
 	http.HandleFunc("/", logup)
 	http.HandleFunc("/login", login)
 
